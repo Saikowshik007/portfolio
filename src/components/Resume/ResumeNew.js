b602324e35b76fc09452845429d02fde93a61132
Saikowshik007 <58332764+Saikowshik007@users.noreply.github.com>
2023-08-18 14:03:58 -0400

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Sai_Kowshik_SE1.pdf";
+import pdf from "../../Assets/../Assets/Sai_Kowshik_AAnanthula.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
